372ddbf37972a3e35c12ab5a5b703b59dfcad3f6
Bobby St. Jacques <mobappdevguy@gmail.com>
2017-12-21 22:00:37 -0500

    Implemented pen colors. And probably some other stuff.

--- src/turtle/turtle.java
+++ src/turtle/turtle.java
@@ -1,3 +1,9 @@
+/*
+ * JavaFX turtle implementation. Provides an animated turtle object that can
+ * be programmatically controlled to draw pictures using a turtle.
+ *
+ * @author Bobby St. Jacques
+ */
 package turtle;
 
 import com.sun.javafx.application.PlatformImpl;
@@ -17,41 +23,117 @@ import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.util.ArrayList;
+import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A JavaFX implementation of Turtle Graphics.
+ */
 public class turtle {
-    private static final double WIDTH = 500;
-    private static final double HEIGHT = 500;
+    /**
+     * The default width of the turtle's world in pixels.
+     */
+    private static final double WIDTH = 700;
 
-    private static final double PIXELS_PER_SECOND = 100;
+    /**
+     * The default height of the turtle's world.
+     */
+    private static final double HEIGHT = 700;
 
-    private static final double DURATION = 1000;
+    /**
+     * Default speed in pixels per second.
+     */
+    private static final double PIXELS_PER_SECOND = 200;
 
+    /**
+     * The default speed at which the turtle turns in degrees per second.
+     */
+    private static final double DEGREES_PER_SECOND = 180;
+
+    /**
+     * The static (singleton) turtle.
+     */
     public static final turtle turtle = new turtle();
 
+    /**
+     * The polygon that represents the turtle's arrowhead.
+     */
     private Shape turtleShape;
+
+    /**
+     * The angle that the turtle is currently facing.
+     */
     private double angle;
+
+    /**
+     * The turtle's current x-coordinate position (this is the real x
+     * coordinate with 0,0 in the top left of the window, NOT the turtle
+     * coordinate with 0,0 in the center of the window).
+     */
     private double x;
+
+    /**
+     * The turtle's current y-coordinate position (this is the real y
+     * coordinate with 0,0 in the top left of the window, NOT the turtle
+     * coordinate with 0,0 in the center of the window).
+     */
     private double y;
 
+    /**
+     * The JavaFX groupt to which all shapes are added to be rendered.
+     */
     private Group root;
 
+    /**
+     * Helper {@link Thread} that insures that animations are completed one at
+     * a time, in order.
+     */
     private Animator animator;
 
+    /**
+     * The current state of the pen; true if the pen is down.
+     */
     private boolean penDown;
 
+    /**
+     * The color used for the turtle's pen.
+     */
+    private Color penColor;
+
+    /**
+     * The speed at which the turtle moves.
+     */
+    private int speed;
+
+    /**
+     * Indicates whether or not the tracer is enabled; if true, the turtle
+     * draws at its configured speed. If disabled, the turtle draws nearly
+     * instantly.
+     */
+    private boolean tracer;
+
+    /**
+     * The current visibility of the turtle's world; false if the turle's
+     * world is not currently being displayed.
+     */
     private boolean notDisplayed;
 
+    /**
+     * Initialized the turtle with its default settings.
+     */
     private turtle() {
-
+        // facing east
         angle = 0;
+
+        //in the center of the canvas
         x = WIDTH / 2;
         y = HEIGHT / 2;
 
+        // the group used to draw all of the various lines and shapes
         root = new Group();
 
+        // initialize the turtle to be an arrow head.
         turtleShape = new Polygon(0, 0, -3.75, -5, 10, 0, -3.75, 5);
         turtleShape.setFill(Color.BLACK);
         turtleShape.setTranslateX(250);
@@ -60,6 +142,9 @@ public class turtle {
         root.getChildren().add(turtleShape);
 
         penDown = true;
+        penColor = Color.BLACK;
+
+        tracer = true;
 
         animator = new Animator();
         Thread animationThread = new Thread(animator);
@@ -67,6 +152,86 @@ public class turtle {
         animationThread.start();
 
         notDisplayed = true;
+    }
+
+    /**
+     * Enables or disables the tracer. If the tracer is enabled, the turtle's
+     * drawings are animated. If the tracer is disabled, the drawings are
+     * essentially instantaneous.
+     *
+     * @param tracer True if the tracer should be enabled, false otherwise.
+     */
+    public void tracer(boolean tracer) {
+        this.tracer = tracer;
+    }
+
+    /**
+     * Returns the tracer's current state.
+     *
+     * @return True if the tracer is enabled, false otherwise.
+     */
+    public boolean tracer() {
+        return tracer;
+    }
+
+    /**
+     * Sets the pen's color the the specified RGB values using integer values
+     * between 0 and 255.
+     *
+     * @param red The value for the red channel.
+     * @param green The value for the green channel.
+     * @param blue The value for the red channel.
+     */
+    public void penColor(int red, int green, int blue) {
+        if(red < 0 || red > 255 ||
+                green < 0 || green > 255 ||
+                blue < 0 || blue > 255 ) {
+            throw new IllegalArgumentException("bad color sequence: (" +
+                    red + ", " + green + ", " + blue + ")");
+        }
+
+        double realRed = (double)red / 255;
+        double realGreen = (double)green / 255;
+        double realBlue = (double)blue / 255;
+
+        penColor(realRed, realGreen, realBlue);
+    }
+
+    /**
+     * Sets the pen's color to the specified RGB values using decimal values
+     * between 0.0 and 1.0.
+     *
+     * @param red The value for the red channel.
+     * @param green The value for the green channel.
+     * @param blue The value for the red channel.
+     */
+    public void penColor(double red, double green, double blue) {
+        if(red < 0 || red > 1 ||
+                green < 0 || green > 1 ||
+                blue < 0 || blue > 1 ) {
+            throw new IllegalArgumentException("bad color sequence: (" +
+                red + ", " + green + ", " + blue + ")");
+        }
+
+        penColor = new Color(red, green, blue, 1.0);
+    }
+
+    /**
+     * Sets the pen's color to the color matching the specified string.
+     *
+     * @param color The name of the color to which the pen color should be
+     *              set.
+     */
+    public void penColor(String color) {
+        String upperColor = color.toUpperCase();
+
+        try {
+            Field theColor = Color.class.getField(color.toUpperCase());
+            penColor = (Color)theColor.get(null);
+        }
+        catch(Exception e) {
+            throw new IllegalArgumentException("bad color string: " + color);
+        }
     }
 
     public void pd() {
@@ -120,9 +285,11 @@ public class turtle {
     public void right(double degrees) {
         angle += degrees;
 
+        double duration = degrees / DEGREES_PER_SECOND * 1000;
+
         display();
         Timeline animation = new Timeline(
-                new KeyFrame(Duration.millis(DURATION),
+                new KeyFrame(Duration.millis(duration),
                 new KeyValue(turtleShape.rotateProperty(), angle)));
         animator.addAnimation(animation);
     }
@@ -134,9 +301,11 @@ public class turtle {
     public void left(double degrees) {
         angle -= degrees;
 
+        double duration = degrees / DEGREES_PER_SECOND * 1000;
+
         display();
         Timeline animation = new Timeline(
-                new KeyFrame(Duration.millis(DURATION),
+                new KeyFrame(Duration.millis(duration),
                 new KeyValue(turtleShape.rotateProperty(), angle)));
         animator.addAnimation(animation);
     }
@@ -166,18 +335,20 @@ public class turtle {
             line.setStroke(Color.TRANSPARENT);
 
             animation.getKeyFrames().add(new KeyFrame(Duration.ONE,
-                    new KeyValue(line.strokeProperty(), Color.BLACK)));
+                    new KeyValue(line.strokeProperty(), penColor)));
 
             root.getChildren().add(line);
             keyValues[2] = new KeyValue(line.endXProperty(), realX);
             keyValues[3] = new KeyValue(line.endYProperty(), realY);
         }
 
-        x = realX;
-        y = realY;
+        double duration = getDuration(x, y, realX, realY);
 
         animation.getKeyFrames().add(
-                new KeyFrame(Duration.millis(DURATION), keyValues));
+                new KeyFrame(Duration.millis(duration), keyValues));
+
+        x = realX;
+        y = realY;
 
         animator.addAnimation(animation);
     }
@@ -225,7 +396,7 @@ public class turtle {
         @Override
         public void start(Stage primaryStage) {
             Scene scene = new Scene(turtle.root, WIDTH, HEIGHT,
-                    Color.GHOSTWHITE);
+                    Color.WHITE);
 
             primaryStage.setTitle("JTurtle!");
             primaryStage.setScene(scene);
@@ -242,10 +413,12 @@ public class turtle {
     private static class Animator implements Runnable,
             EventHandler<ActionEvent> {
         private final List<Animation> queue;
+        private boolean finished;
         private boolean running;
 
         Animator() {
             queue = new LinkedList<>();
+            finished = true;
             running = true;
         }
 
@@ -253,7 +426,8 @@ public class turtle {
         public void run() {
             synchronized(queue) {
                 while(running) {
-                    if (queue.size() > 0) {
+                    if(finished && queue.size() > 0) {
+                        finished = false;
                         Animation next = queue.remove(0);
                         next.setOnFinished(this);
                         next.play();
@@ -271,6 +445,7 @@ public class turtle {
         @Override
         public void handle(ActionEvent event) {
             synchronized(queue) {
+                finished = true;
                 queue.notify();
             }
         }
