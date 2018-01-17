072129a42ddb4226dfc469eed3fc3241c391eba3
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2018-01-17 15:00:15 -0500

    Added the bug fix that prevents locking up on Mac.

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import static turtle.Turtle.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("blah");
+        Turtle.setWorldCoordinates(0, 0, 0, 0);
+        System.out.println("bar");
+        Turtle.title("Foo");
+        System.out.println("foo");
+    }
+}
