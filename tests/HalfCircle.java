2c5e741eee3bf16eb261bbbbf29da8f8ef2cfed6
rjsvcs <rjsvcs@rit.edu>
2019-06-04 14:34:40 -0400

    added a half circle drawing test. so slow.

---
+++ tests/HalfCircle.java
@@ -0,0 +1,12 @@
+import static turtle.Turtle.*;
+
+public class HalfCircle {
+    public static void main(String[] args) {
+        Turtle.down();
+        Turtle.forward(100);
+        Turtle.left(90);
+        Turtle.circle(100, 180);
+        Turtle.left(90);
+        Turtle.forward(100);
+    }
+}
